dede6825bd1ec5439d2c1dbfc4851a3b53a4ed05
martondurucsko <durucsko.marci@gmail.com>
2024-05-12 20:59:46 +0200

    feat : create game manager test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "graphics.hpp"
 
+#include "gamemanager.hpp"
 #include "widget.hpp"
 #include "numericupdown.hpp"
 #include "selector.hpp"
@@ -45,62 +46,8 @@ int main()
 
     vector<Widget*> w;
 
-    //Widget 1.
-    NumericUpDown* n1 = new NumericUpDown(w, 50, 95, 200, 50, 0, 1, -10, 10);
+    GameManager* gm = new GameManager(w, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    //Widget 2.
-    NumericUpDown* n2 = new NumericUpDown(w, 260, 95, 200, 50, 1000, 10);
-
-    //Selector 1.
-    Selector* s1 = new Selector(w, 50, 155, 200, 50, {"Alma", "Korte", "Barack", "Szilva", "Fuge", "Gomba", "Dinnye", "Malna", "Bodza"});
-
-    //Selector 2.
-    Selector* s2 = new Selector(w, 260, 155, 200, 50, {"Kutya", "Macska", "Szarvas", "Tehen"});
-
-
-    //Write to file button
-    new Button(w, 10, (WINDOW_HEIGHT - 60), 50, 50, "Save", [&]() {
-        std::ofstream output("ertekek.txt");
-        if (output.is_open()) {
-            output << "Raw values of widgets:" << std::endl << std::endl;
-            output << "1. Widget " << std::endl << n1->getValue() << std::endl;
-            output << "2. Widget " << std::endl << n2->getValue() << std::endl;
-            output << "3. Widget " << std::endl << s1->getValue() << std::endl;
-            output << "4. Widget " << std::endl << s2->getValue() << std::endl << std::endl;
-
-            output << "toString values of widgets:" << std::endl << std::endl;
-            output << "1. Widget " << std::endl << n1->to_string() << std::endl;
-            output << "2. Widget " << std::endl << n2->to_string() << std::endl;
-            output << "3. Widget " << std::endl << s1->to_string() << std::endl;
-            output << "4. Widget " << std::endl << s2->to_string() << std::endl;
-
-
-            output.close();
-        }
-    });
-
-    //Show/Hide the 1. Selector
-    Button* b1 = new Button(w, WINDOW_WIDTH - 70, WINDOW_HEIGHT - 50, 60, 40, "Hide", [&]() {
-        s1->setVisible(!s1->getVisible());
-        b1->setText(s1->getVisible() ? "Hide" : "Show");
-    });
-
-    //Enable/Disable the 1. Selector
-    Button* b2 = new Button(w, WINDOW_WIDTH - 155, WINDOW_HEIGHT - 50, 80, 40, "Disable", [&]() {
-        s1->setEnable(!s1->getEnable());
-        b2->setText(s1->getEnable() ? "Disable" : "Enable");
-    });
-
-    //Set new values for the 1. Selector
-    new Button(w, WINDOW_WIDTH - 210, WINDOW_HEIGHT - 50, 50, 40, "Set", [&]() {
-        s1->setOptions({"Banan", "Mandarin", "Citrom", "Szolo", "Eper", "Kokusz"});
-    });
-
-    //Remove element from the 1. Selector by index. Value given by the 1. NumericUpDown
-    new Button(w, WINDOW_WIDTH - 295, WINDOW_HEIGHT - 50, 80, 40, "Remove", [&]() {
-        std::cout << s1->remove(n1->getValue()) << std::endl;
-
-    });
 
     //Draw scene before first event
     for (Widget* wg : w) {
